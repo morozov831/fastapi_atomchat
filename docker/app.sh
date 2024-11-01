785c45fb9e5a5e6b68358c246ceaa42b6c2e6b45
morozov831 <artiom.morozov.2001@mail.ru>
2024-11-01 13:58:19 +0300

    up

--- docker/app.sh
+++ docker/app.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 alembic upgrade head
-python seed.py
-cd app
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
 
+python seed.py
+
+cd app
+
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
 
